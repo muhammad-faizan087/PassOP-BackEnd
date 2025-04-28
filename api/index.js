48ccbee116d3a299419adc40b0493b39b841360b
Muhammad Faizan <m.faizan11f@gmail.com>
2025-04-28 12:55:50 +0500

    final changes

--- api/index.js
+++ api/index.js
@@ -27,7 +27,7 @@ app.use("/login", login);
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173", "https://pass-op-front-end.vercel.app/"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
   })
