6c79fdb32050ca647ee4f113734ca438b2bb40e4
Muhammad Faizan <m.faizan11f@gmail.com>
2025-04-28 13:10:26 +0500

    Final changes

--- api/index.js
+++ api/index.js
@@ -30,6 +30,7 @@ app.use(
     origin: ["http://localhost:5173", "https://pass-op-front-end.vercel.app/"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
   })
 );
 app.use(bodyParser.json());
